bff5287a8a36b53c5c0c66896eeb6fbf1bb66c5b
yinping xie <yinping.xie@renren-inc.com>
2020-05-31 00:02:05 +0800

    21

---
+++ src/main/java/com/xie/leetcode/mergeTwoLists21/Solution.java
@@ -0,0 +1,54 @@
+package com.xie.leetcode.mergeTwoLists21;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode res = s.mergeTwoLists(l1, l2);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode tmp = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tmp.next = l1;
+                l1 = l1.next;
+            } else {
+                tmp.next = l2;
+                l2 = l2.next;
+            }
+            tmp = tmp.next;
+        }
+        if (l1 != null) {
+            tmp.next = l1;
+        }
+        if (l2 != null) {
+            tmp.next = l2;
+        }
+        return head.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ src/main/java/com/xie/leetcode/mergeTwoLists21/Solution2.java
@@ -0,0 +1,48 @@
+package com.xie.leetcode.mergeTwoLists21;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode res = s.mergeTwoLists(l1, l2);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
